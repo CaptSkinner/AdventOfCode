1712bc9fdfe017f0ccd33a7a4b8d5b2893e614c6
Gary Skinner <garyskinner@me.com>
2026-02-17 17:54:49 +1100

    format changes

--- 2015/day3.py
+++ 2015/day3.py
@@ -1,21 +1,20 @@
+# Open file
 file = "day3.txt"
 with open(file, 'r') as file:
     movements = file.read()
 
-# house_location
+# House start location
 x, y = 0, 0
 house_stops = set()
 house_stops.add((x, y))
 
+# Movements
 for movement in movements:
-    if movement == ">":
-        x += 1
-    if movement == "<":
-        x -= 1
-    if movement == "^":
-        y += 1
-    if movement == "v":
-        y -= 1
+    if movement == ">": x += 1
+    if movement == "<": x -= 1
+    if movement == "^": y += 1
+    if movement == "v": y -= 1
     house_stops.add((x, y))
-print(house_stops)
+
+# Amount of unique houses visited
 print(len(house_stops))
