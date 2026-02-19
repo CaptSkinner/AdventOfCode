30e823e0ed13e9158056b3d97bf0e9f1f17d407c
Gary <garyskinner@me.com>
2026-02-19 12:53:21 +1100

    Day7 part 1 completed

--- 2015/day7.py
+++ 2015/day7.py
@@ -1,20 +1,55 @@
-## Work in Progress
-
 file = "../Inputs/day7.txt"
 with open(file, 'r') as file:
-    instructions = file.read()
-print(instructions.split("\n"))
-print(instructions.split("\n")[0].split(" "))
+    instructions = file.read().split("\n")
 
-# Examples
-# if "AND" in instructions:
-#     destination = source1 & source2
-# if "OR" in instructions:
-#     destination = source1 | source2
-# if "LSHIFT" in instructions:
-#     destination = source1 << amount
-# if "RSHIFT" in instructions:
-#     destination = source1 >> amount
-# if "NOT" in instructions:
-#     destination = ~source1 & 0xFFFF
+wire_diagram = {}
 
+def get_value(input_str):
+    if input_str.isdigit():
+        return int(input_str)
+    else:
+        return wire_diagram[input_str]
+
+def install_wires(instructions):
+    while instructions:
+        remaining = []
+
+        for instruction in instructions:
+            parts = instruction.split(" -> ")
+            source = parts[0]
+            destination = parts[1]
+
+            try:
+                if "AND" in source:
+                    sources = source.split(" AND ")
+                    wire_diagram[destination] = (get_value(sources[0])) & (get_value(sources[1]))
+
+                elif "OR" in source:
+                    sources = source.split(" OR ")
+                    wire_diagram[destination] = (get_value(sources[0])) | (get_value(sources[1]))
+
+                elif "LSHIFT" in source:
+                    parts_op = source.split(" LSHIFT ")
+                    wire_diagram[destination] = get_value(parts_op[0]) << int(parts_op[1])
+
+                elif "RSHIFT" in source:
+                    parts_op = source.split(" RSHIFT ")
+                    wire_diagram[destination] = get_value(parts_op[0]) >> int(parts_op[1])
+
+                elif "NOT" in source:
+                    source1 = source.replace("NOT ", "")
+                    wire_diagram[destination] = ~(get_value(source1)) & 0xFFFF
+
+                else:
+                    val = get_value(source)
+                    wire_diagram[destination] = val
+
+            except (KeyError, ValueError):
+                remaining.append(instruction)
+
+        instructions = remaining
+
+
+
+install_wires(instructions)
+print(wire_diagram["a"])
