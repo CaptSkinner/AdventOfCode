07510a5a725c551f54319beb16a6fc2c6d97963e
Gary <garyskinner@me.com>
2026-02-20 13:18:25 +1100

    Day8 completed

--- 2015/day8.py
+++ 2015/day8.py
@@ -1,26 +1,51 @@
-# Work in Progress
+import time
 
 file = "../Inputs/day8.txt"
 with open(file, 'r') as file:
-    string_literals = file.read().split("\n")
+    strings_list = file.read().split("\n")
 
-# get total amount of characters
-strings_count = 0
-for strings in string_literals:
-    strings_count += len(strings)
-
-# get alpha characters len only
-character_only_list = []
-for strings in string_literals:
-    for letters in strings:
-        if letters.isalpha():
-            character_only_list.append(letters)
+# Get total literal characters
+strings_literal = 0
+for string in strings_list:
+    strings_literal += len(string)
 
 
+def count_mem_char(strings_list):
+    total_memory = 0
+    total_strings = len(strings_list)
+
+    for i, strings in enumerate(strings_list):
+        string = strings[1:-1]
+        char_count = 0
+        index = 0
+
+        while index < len(string):
+            time.sleep(.001)
+            if string[index] == "\\":
+                if string[index + 1] == "\\":
+                    char_count += 1
+                    index += 2
+                elif string[index + 1] == '"':
+                    char_count += 1
+                    index += 2
+                elif string[index + 1] == "x":
+                    char_count += 1
+                    index += 4
+            else:
+                char_count += 1
+                index += 1
+
+        total_memory += char_count
+
+        # Print progress every 10%
+        if (i + 1) % (total_strings // 10) == 0:
+            percent = ((i + 1) / total_strings) * 100
+            print(f"Progress: {percent:.0f}%")
+
+    return total_memory
 
 
-print(f"strings_count = {strings_count}")
-print(string_literals)
-print()
-print(f"character_only_list = {character_only_list}")
-print(print(f"character_only_list length = {len(character_only_list)}"))
+strings_memory = count_mem_char(strings_list)
+print(f"Literal: {strings_literal}")
+print(f"Memory: {strings_memory}")
+print(f"Difference: {strings_literal - strings_memory}")
