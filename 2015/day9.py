895fc84563777c7043f0b1e5c4f017264e6e98b3
Gary <garyskinner@me.com>
2026-02-24 15:53:24 +1100

    day 9 WIP

--- 2015/day9.py
+++ 2015/day9.py
@@ -4,15 +4,42 @@ file = "../Inputs/day9.txt"
 with open(file, 'r') as file:
     routes = file.read().split("\n")
 
-flight_array = {}
+flights_array = {}
+city_list = set()
+
 for route in routes:
+    # capture flight routes
     flight = [route.split(" = ")[0]]
+    # capture flight distance
     distance = int(route.split(" = ")[1])
+
     for destinations in flight:
+        # capture departure and destination cities
         departure = str(destinations.split(" to ")[0])
         destination = str(destinations.split(" to ")[1])
-        flight_array[departure, destination] = distance
-        flight_array[destination, departure] = distance
+        # build city total city list
+        city_list.add(departure)
+        city_list.add(destination)
+
+        # building dict keys for flight array
+        if departure not in flights_array:
+            flights_array[departure] = {}
+        if destination not in flights_array:
+            flights_array[destination] = {}
+
+        # populating flight array and total city list
+        flights_array[departure][destination] = distance
+        flights_array[destination][departure] = distance
+        city_list.add(departure)
+        city_list.add(destination)
+
+print(flights_array)
+
+
+
+
+
+
 
 
 
