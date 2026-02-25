8d3afd3b35956f52fc0dc6f55ce5ae06862674b4
Gary <garyskinner@me.com>
2026-02-25 17:15:29 +1100

    Day 10 part 1 completed

--- 2015/day10.py
+++ 2015/day10.py
@@ -1,19 +1,22 @@
-# WIP only working with a single digit on the end
-# need to add guardrail for the index out of range
 input = "3113322113"
 
 def get_las(input):
     output = []
     index = 0
     counter = 1
-    while index + 1 < len(str(input)):
-        while input[index] == input[index + 1]:
+    while index + 1 < len(input):
+        if input[index] == input[index + 1]:
             counter += 1
             index += 1
-        output.append(str(counter) + input[index])
-        index += 1
-        counter = 1
+        else:
+            output.append(str(counter) + input[index])
+            index += 1
+            counter = 1
     output.append(str(counter) + input[index])
     return "".join(output)
 
-print(get_las(input))
+result = input
+for attempts in range(40):
+    result = get_las(result)
+
+print(len(result))
